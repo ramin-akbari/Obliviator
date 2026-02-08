refactoring + fixed backprop bug + batched mm

src/obliviator/base.py
@@ -5,7 +5,7 @@ import numpy as np
 import torch
 from torch.utils.data import DataLoader
 
-from .schemas import UnsupervisedConfig
+from .schemas import ErasureConfig
 from .utils.linalg import RandomFourierFeature, median_heuristic_sigma
 from .utils.misc import mlp_factory, optim_factory
 
@@ -31,23 +31,23 @@ class Obliviator(ABC):
         x: torch.Tensor | np.ndarray,
         s: torch.Tensor | np.ndarray,
         x_test: torch.Tensor | np.ndarray,
-        config: UnsupervisedConfig,
-        device: torch.device,
+        config: ErasureConfig,
         dtype: torch.dtype = torch.float32,
     ) -> None:
 
         self.x = torch.as_tensor(x, dtype=dtype)
         self.s = torch.as_tensor(s, dtype=dtype)
-        self.x_test = torch.as_tensor(x_test, dtype=dtype, device=config.device)
+        self.x_test = torch.as_tensor(x_test, dtype=dtype)
         self.sigma_min = config.sigma_min
+        self.update_batch = config.update_batch
 
         self.tau_x = config.tau_x
         self.tau_z = config.tau_z
 
         self.encoder_config = config.encoder_config
-        self.encoder = mlp_factory(config.encoder_config)
-        self.optim = optim_factory(config.optim_config)
-        self.device = device
+        self.encoder = torch.nn.Identity()
+        self.optim_factory = optim_factory(config.optim_config)
+        self.device = torch.device(config.device)
         self.loader = partial(
             DataLoader,
             batch_size=config.optim_config.batch_size,
@@ -61,8 +61,8 @@ class Obliviator(ABC):
             self.x.shape[1],
             config.drff_min,
             config.drff_max,
-            device,
-            config.resamle_rff_weights_x,
+            self.device,
+            config.resample_x,
             median_heuristic_sigma(self.x, config.sigma_min_x),
         )
 
@@ -70,16 +70,16 @@ class Obliviator(ABC):
             config.encoder_config.out_dim,
             config.drff_min,
             config.drff_max,
-            device,
+            self.device,
             True,
         )
 
         if config.use_rff_s:
             phi_s = _rff_helper(
                 self.s.shape[1],
-                config.drff_min,
+                config.drff_label_min,
                 config.drff_max,
-                device,
+                self.device,
                 False,
                 median_heuristic_sigma(s, config.sigma_min_s),
             )
@@ -88,22 +88,29 @@ class Obliviator(ABC):
     def _loss_embeddings(self, z: torch.Tensor) -> torch.Tensor:
         w = self.encoder(z)
         w = w.div(w.norm(dim=1, keepdim=True))
-        self.phi.sigma = median_heuristic_sigma(w, self.sigma_min)
+        self.phi.change_sigma(median_heuristic_sigma(w, self.sigma_min))
         return self.phi(w)
 
     @torch.no_grad()
-    def get_embeddings(self, z: torch.Tensor) -> torch.Tensor:
-        embd = self.encoder(z)
-        return embd.div_(embd.norm(dim=1, keepdim=True))
+    def get_embeddings(self, z: torch.Tensor, batch: int) -> torch.Tensor:
+        def helper(x: torch.Tensor) -> torch.Tensor:
+            x = self.encoder(x.to(device=self.device, non_blocking=True))
+            return x.div_(x.norm(dim=1, keepdim=True)).cpu()
+
+        return torch.cat(
+            [helper(z_batched) for z_batched in torch.split(z, batch, dim=0)],
+            dim=0,
+        )
 
     def update_encoder(self, in_dim: int, out_dim: int) -> None:
         self.encoder_config.input_dim = in_dim
-        self.encoder_config.hidden_dim = out_dim
-        self.encoder = mlp_factory(self.encoder_config)
 
-    @abstractmethod
-    def _init_encoder(self, epochs: int) -> None:
-        pass
+        if out_dim != self.encoder_config.out_dim:
+            self.phi.change_input_dim(out_dim)
+            self.encoder_config.out_dim = out_dim
+            self.encoder_config.hidden_dim = out_dim
+
+        self.encoder = mlp_factory(self.encoder_config)
 
     @abstractmethod
     def init_erasure(self, tol: float) -> None:

src/obliviator/supervised.py
@@ -11,8 +11,7 @@ class Supervised(Obliviator):
         x: torch.Tensor | np.ndarray,
         s: torch.Tensor | np.ndarray,
         x_test: torch.Tensor | np.ndarray,
-        encoder: torch.nn.Module,
         config: SupervisedConfig,
-        device: torch.device,
+        dtype: torch.dtype = torch.float32,
     ) -> None:
-        super().__init__(x, s, x_test, encoder, config, device)
+        super().__init__(x, s, x_test, config, dtype)

src/obliviator/unsupervised.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 from tqdm import trange
+from typing_extensions import override
 
 from .base import Obliviator
 from .schemas import UnsupervisedConfig
@@ -15,10 +16,11 @@ class Unsupervised(Obliviator):
         s: torch.Tensor | np.ndarray,
         x_test: torch.Tensor | np.ndarray,
         config: UnsupervisedConfig,
-        device: torch.device,
         dtype: torch.dtype = torch.float32,
     ) -> None:
-        super().__init__(x, s, x_test, config, device, dtype)
+        super().__init__(x, s, x_test, config, dtype)
+        self.init_erasure_epochs = config.init_erasure_epochs
+        self.init_erasure_steps = config.init_erasure_steps
 
     def init_erasure(self, tol: float) -> None:
         x = self.phi_x(self.x)
@@ -27,16 +29,19 @@ class Unsupervised(Obliviator):
         self.x = x.sub_(mu).mm(f).cpu()
         self.x_test = self.x_test.sub_(mu).mm(f)
         self.update_encoder(f.shape[1], self.encoder_config.hidden_dim)
+        self._init_encoder(self.init_erasure_epochs)
+        self.z = self.get_embeddings(self.x, self.update_batch)
+        self.x_test = self.get_embeddings(self.x_test, self.update_batch)
 
     def _init_encoder(self, epochs: int) -> None:
         data = self.loader(InitDataset(self.x, self.s))
         pbar = trange(epochs)
-        optimizer = self.optim(self.encoder.parameters())
+        optimizer = self.optim_factory(self.encoder.parameters())
         for _ in pbar:
             for z, s in data:
                 optimizer.zero_grad()
-                z = z.to(self.device)
-                s = s.to(self.device)
+                z = z.to(self.device, non_blocking=True)
+                s = s.to(self.device, non_blocking=True)
                 sc_s = torch.cov(s.T).norm("fro").sqrt()
                 sc_z = torch.cov(z.T).norm("fro").sqrt()
                 w = self._loss_embeddings(z)
@@ -45,3 +50,7 @@ class Unsupervised(Obliviator):
                 loss = hs_z.mul_(self.tau_z) - hs_s
                 loss.backward()
                 optimizer.step()
+
+    @override
+    def solve_evp(self, tol: float) -> None:
+        return super().solve_evp(tol)

src/obliviator/utils/linalg.py
@@ -6,7 +6,26 @@ def cross_cov(x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
     x = x - x.mean(dim=0)
     # technically not needed, only because of floating-point error in x_mean
     y = y - y.mean(dim=0)
-    return (x.T @ y).mul_(1 / x.shape[0])
+    return (x.T @ y).mul(1 / x.shape[0])
+
+
+def batched_cross_cov(
+    x: torch.Tensor, y: torch.Tensor, batch: int, device: torch.device
+) -> torch.Tensor:
+    mu_x = x.mean(dim=0).to(device=device)
+    mu_y = y.mean(dim=0).to(device=device)
+    batched_x = torch.split(x, batch)
+    batched_y = torch.split(y, batch)
+    Cxy = torch.zeros(x.shape[1], y.shape[1], device=device)
+
+    for x, y in zip(batched_x, batched_y):
+        x = x.to(device=device, non_blocking=True)
+        y = y.to(device=device, non_blocking=True)
+        x = x.sub(mu_x)
+        y = y.sub(mu_y)
+        Cxy = Cxy.addmm(x.T, y)
+
+    return Cxy.div(x.shape[0])
 
 
 def select_top_k_eigvec(x: torch.Tensor, rtol: float, atol: float) -> torch.Tensor:
@@ -37,30 +56,42 @@ def null_pca(
 class RandomFourierFeature:
     def __init__(
         self,
-        dim_x: int,
+        d_in: int,
         dim_rff: int,
         resample: bool,
         sigma_rff: float,
         device: torch.device,
     ) -> None:
         dim_rff = dim_rff // 2
-        self.sigma = sigma_rff
+        self._sigma = sigma_rff
+        self._d_in = d_in
 
         def helper_weight_sampler() -> torch.Tensor:
-            return torch.randn(dim_x, dim_rff, device=device).div_(self.sigma)
+            return torch.randn(self._d_in, dim_rff, device=device).div_(self._sigma)
 
         self.w = helper_weight_sampler()
         self.sampler = helper_weight_sampler
         self.c = (1.0 / torch.tensor(dim_rff, device=device)).sqrt()
+        self.resample = resample
         if resample:
-            self.map_ptr = self.map
-        else:
             self.map_ptr = self.resample_map
+        else:
+            self.map_ptr = self.map
 
     def map(self, x: torch.Tensor | ndarray) -> torch.Tensor:
-        x = torch.as_tensor(x).to(device=self.w.device, dtype=self.w.dtype)
+        x = torch.as_tensor(x, dtype=self.w.dtype).to(device=self.w.device)
         rff = x @ self.w
         return torch.cat([rff.sin(), rff.cos()], dim=1).mul_(self.c)
+
+    def change_input_dim(self, d_in: int) -> None:
+        self._d_in = d_in
+        if not self.resample:
+            self.w = self.sampler()
+
+    def change_sigma(self, sigma: float) -> None:
+        self._sigma = sigma
+        if not self.resample:
+            self.w = self.sampler()
 
     def resample_map(self, x: torch.Tensor | ndarray) -> torch.Tensor:
         self.w = self.sampler()